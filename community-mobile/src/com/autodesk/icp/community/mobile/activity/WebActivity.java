efbe0a659220c5f6dd2e0415ddcfdb8e2528593a
wuol <oliver.wu@autodesk.com>
2015-04-12 09:14:39 +0800

    Initiate mobile skeleton which includes the launcher, login, authentication features.

---
+++ community-mobile/src/com/autodesk/icp/community/mobile/activity/WebActivity.java
@@ -0,0 +1,76 @@
+package com.autodesk.icp.community.mobile.activity;
+
+import android.app.Activity;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.autodesk.icp.community.mobile.util.WebAppInterface;
+
+public class WebActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webmain);
+
+        webView = (WebView)findViewById(R.id.mainWebView);
+
+        webView.loadUrl("http://10.148.252.16:8080");
+
+        // myWebView.loadUrl("file:///android_asset/www/test2.html");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // return true;
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
