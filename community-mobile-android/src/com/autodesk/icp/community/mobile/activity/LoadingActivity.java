a6785fd984f827084ea89c5985c1cea5e3d5d23d
wuol <oliver.wu@autodesk.com>
2015-05-18 21:27:01 +0800

    Update dummy code.

---
+++ community-mobile-android/src/com/autodesk/icp/community/mobile/activity/LoadingActivity.java
@@ -0,0 +1,24 @@
+package com.autodesk.icp.community.mobile.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+/*        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, Whatsnew.class);
+                startActivity(intent);
+                LoadingActivity.this.finish();                
+            }
+        }, 200);*/
+    }
+}
---
+++ community-mobile-android/src/com/autodesk/icp/community/mobile/util/DataDefine.java
@@ -0,0 +1,57 @@
+package com.autodesk.icp.community.mobile.util;
+
+public class DataDefine {
+	
+	public static final String TABLE_USER = "Tab_User";
+	public static final String TABLE_CUSTOMER = "Tab_Customer";
+	
+	//User Table
+	public static String ID = "id";
+	public static final String USERTYPE = "usertype";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String TELEPHONE = "telephone";
+    public static final String EMAIL = "email";
+    
+    //Customer Table
+    public static final String SEX = "sex";
+    public static final String MANAGERID = "managerId";
+    public static final String CUSTOMERATTRIBUTE = "customerAttribute";
+    
+    //define action
+    public static final String ACTION="action";
+    public static final String ADD="add";
+    public static final String DISPLAY="display";
+    
+    //define material related
+    public static final String NUMBER="number";
+    public static final String MATERIALNAME="MaterialName";
+    public static final String MATERIALQUANTITY="MaterialQuantity";
+    public static final String MATERIALNAMEINSERVER="materialsName";
+    public static final String MATERIALIDINSERVER="materialsId";
+    public static final String MATERIALQUANTITYINSERVER="quantity";
+    
+    //define experiment related
+    public static final String EXPERIMENTNAME="ExperimentName";
+    public static final String EXPERIMENTREMARK="ExperimentRemark";
+    public static final String EXPERIMENTREMARKABSTRACT="ExperimentRemarkAbstract";
+    public static final String EXPERIMENTATTACHNAME="ATTACHNAME";
+    public static final String EXPERIMENTATTACHID="ATTACHID";
+    public static final String EXPERIMENTID="ExperimentID";
+    public static final String EXPERIMENTSEPARATOR="-pmfdd-";
+    
+    //define item related
+    public static final String ITEMNAME="ItemName";
+    public static final String ITEMID="ItemID";
+    public static final String PROJECTNAME="ProjectName";
+    public static final String PROJECTID="ProjectID";
+    
+    public static final String IMAGEFILEPATH="ImageFilePath";
+    public static final String ISLOCALIMAGE="IsLocalImage";
+    public static final String ISNotLOCALIMAGE="IsNotLocalImage";
+    
+    
+    
+	
+
+}
