99304a8777a38afd26f86e6568255692bce386b4
wuol <oliver.wu@autodesk.com>
2015-05-19 18:12:50 +0800

    Make the swipe delete working.

--- community-mobile-android/app/src/main/java/com/autodesk/icp/community/mobile/ui/NotificationRecyclerViewAdapter.java
+++ community-mobile-android/app/src/main/java/com/autodesk/icp/community/mobile/ui/NotificationRecyclerViewAdapter.java
@@ -4,9 +4,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 
+import com.autodesk.icp.community.common.util.Utils;
 import com.autodesk.icp.community.mobile.activity.R;
 import com.daimajia.swipe.SwipeLayout;
 
@@ -21,9 +20,58 @@ public class NotificationRecyclerViewAdapter extends RecyclerView.Adapter<Notifi
     }
 
     @Override
-    public NotificationViewHolder onCreateViewHolder(ViewGroup viewGroup, final int i) {
+    public NotificationViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.notification_card, viewGroup, false);
 
+        initSwip(v);
+
+        return new NotificationViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final NotificationViewHolder viewHolder, int pos) {
+        final NotificationCard card = notificationCards.get(pos);
+
+        viewHolder.mTitleTextView.setText(card.getTitle());
+        viewHolder.mDesTextView.setText(card.getDescription());
+        viewHolder.mDateTextView.setText(Utils.formatDataInUTC(Utils.parseDate(card.getTimestamp()),"yyyy-mm-dd"));
+        if (card.isRead()) {
+            viewHolder.setNotificationReaded();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return notificationCards.size();
+    }
+
+    public void remove(NotificationCard card) {
+        int position = getItemPosition(card);
+        if (position != -1) {
+            notificationCards.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public void add(NotificationCard card, int position) {
+        notificationCards.add(position, card);
+        notifyItemInserted(position);
+    }
+
+    public int getItemPosition(NotificationCard card) {
+        return notificationCards.indexOf(card);
+    }
+
+    public NotificationCard getItemAtPosition(int position) {
+        return notificationCards.get(position);
+    }
+
+    public void refresh() {
+        notifyDataSetChanged();
+    }
+
+
+    private void initSwip(View v) {
         SwipeLayout swipeLayout = (SwipeLayout) v.findViewById(R.id.notifcation_swipeLayout);
 
 //set show mode.
@@ -63,57 +111,5 @@ public class NotificationRecyclerViewAdapter extends RecyclerView.Adapter<Notifi
                 //when user's hand released.
             }
         });
-
-        Button del = (Button)v.findViewById(R.id.notification_delete_button);
-        del.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                remove(getItemAtPosition(i));
-            }
-        });
-
-        return new NotificationViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(final NotificationViewHolder viewHolder, int pos) {
-        final NotificationCard card = notificationCards.get(pos);
-
-        viewHolder.mTitleTextView.setText(card.getTitle());
-        viewHolder.mDesTextView.setText(card.getDescription());
-        viewHolder.mDateTextView.setText(card.getTimestamp());
-        if (card.isRead()) {
-            viewHolder.setNotificationReaded();
-        }
-    }
-
-    @Override
-    public int getItemCount() {
-        return notificationCards.size();
-    }
-
-    public void remove(NotificationCard card) {
-        int position = getItemPosition(card);
-        if (position != -1) {
-            notificationCards.remove(position);
-            notifyItemRemoved(position);
-        }
-    }
-
-    public void add(NotificationCard card, final int position) {
-        notificationCards.add(position, card);
-        notifyItemInserted(position);
-    }
-
-    public int getItemPosition(NotificationCard card) {
-        return notificationCards.indexOf(card);
-    }
-
-    public NotificationCard getItemAtPosition(int position) {
-        return notificationCards.get(position);
-    }
-
-    public void refresh() {
-        notifyDataSetChanged();
     }
 }
